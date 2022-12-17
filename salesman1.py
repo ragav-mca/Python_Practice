6dfa4641ec7940757e574da367ffd9f6e73645fe
ragav-mca <110483081+ragav-mca@users.noreply.github.com>
2022-12-18 03:53:59 +0530

    Update salesman1.py

--- salesman1.py
+++ salesman1.py
@@ -12,14 +12,21 @@ else:
     print("No Increment")
 phone_sold = int(input("Enter the no of phones sold: "))
 if(phone_sold >= 10):
-    x = phone_sold / 10
-    y = 13/100 * x 
-    commission = x + y
+    commission = 13/100 * 10 
     print("Commission:",commission)
 else:
     print("No Commission")
-if(phone_sold >= 10 or exp >= 1 or leave_count >=1):
+if(phone_sold >= 10 and exp >= 1 and leave_count >=1):
    total_pay = base_pay * commission + increment - lop
    print("Total amount of salary:",total_pay)
 else:
     print("Total amount of salary:",base_pay)
+
+Output:
+Enter the year of experience: 1
+Enter the no of days leave: 2
+Loss of pay is:  400
+Increment: 2000
+Enter the no of phones sold: 30
+Commission: 1.3
+Total amount of salary: 14600.0
